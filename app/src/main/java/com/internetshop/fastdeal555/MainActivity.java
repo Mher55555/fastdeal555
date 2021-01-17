1174a437f7d2cce692c63c72c00815cb8dfc6855
Your Name <you@example.com>
2021-01-17 21:47:38 +0300

    'Init'

---
+++ app/src/main/java/com/internetshop/fastdeal555/MainActivity.java
@@ -0,0 +1,37 @@
+package com.internetshop.fastdeal555;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    public Button joinButton,  loginButton ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        joinButton = (Button) findViewById(R.id.main_joinbutton);
+        loginButton = (Button) findViewById(R.id.main_loginbutton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent( MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        joinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent( MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
